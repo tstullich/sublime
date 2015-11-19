37cf0ff4e35e70e53931c470ce0e9a44272b35ca
Tim Stullich <timstullich@gmail.com>
2015-11-18 21:32:44 -0800

    Started creating basic layout hierarchy

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+var jade = require('jade');
+var mount = require('koa-mount')
+var koa = require('koa')
+
+function *index(next) {
+    yield next;
+    // compile
+    var fn = jade.compileFile('index.jade');
+    this.body = fn({pageTitle:'Sublime Handmades', youAreUsingJade:true});
+}
+
+var app = koa();
+
+app.use(mount(index));
+app.listen(3000);
+console.log('listening on port 3000');
