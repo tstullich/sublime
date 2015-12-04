0b8f84c2ba3d692c88fcdfb820df990d7dd9ff27
Tim Stullich <timstullich@gmail.com>
2015-12-04 13:51:45 -0800

    Renamed some variables to be more in line with the camel-case convention

--- js/dbclient.js
+++ js/dbclient.js
@@ -24,20 +24,20 @@ function getID() {
 }
 
 // Function to add a new item to the database. Returns boolean indicating whether or not operation succeeded
-dbclient.setItem = function*(img_name, title, testimonial) {
+dbclient.setItem = function*(imgName, title, testimonial) {
     // Test our inputs before continuing
-    if (typeof img_name === undefined || typeof title === undefined) {
+    if (typeof imgName === undefined || typeof title === undefined) {
         console.log('[REDIS] ERROR: Not enough parameters defined. Cannot perform SET.');
         return false;
     }
     testimonial = typeof testimonial !== 'undefined' ? testimonial : '';
-    console.log('HMSET Params: %s %s %s', img_name, title, testimonial);
+    console.log('HMSET Params: %s %s %s', imgName, title, testimonial);
 
     var client = redis.createClient(config.port, config.address);
     var itemID = yield getID();
     console.log('[REDIS] New Item ID: %s', itemID);
     // Testimonials are optional so we need to determine how we set the data
-    return client.hmsetAsync(itemID, 'img_name', img_name, 'title', title, 'testimonial', testimonial)
+    return client.hmsetAsync(itemID, 'imgName', imgName, 'title', title, 'testimonial', testimonial)
     .then(function() {
         return client.lpushAsync('item_list', itemID) // Need to push new ID to list of items
         .then(function() {
@@ -56,20 +56,21 @@ dbclient.setItem = function*(img_name, title, testimonial) {
     });
 }
 
-dbclient.getItem = function*(item_id) {
-    if (typeof item_id === undefined) {
+// Finds a single item in database based on the item id
+dbclient.getItem = function*(itemID) {
+    if (typeof itemID === undefined) {
         console.log('[REDIS] ERROR: Not enough parameters defined. Cannot perform GET');
     }
 
     var client = redis.createClient(config.port, config.address);
-    return client.hmgetAsync(item_id, 'img_name', 'title', 'testimonial')
+    return client.hmgetAsync(itemID, 'imgName', 'title', 'testimonial')
     .then(function(reply) {
         console.log('[REDIS] GET ITEM reply: ' + reply);
         client.quit();
 
         // This is terrible and I hate it. Need to change this ASAP
         var result = {};
-        result['img_name'] = reply[0];
+        result['imgName'] = reply[0];
         result['title'] = reply[1];
         result['testimonial'] = reply[2] !== null ? reply[2] : 'N/A';
         return JSON.stringify(result); // wrapping our reply to JSON
